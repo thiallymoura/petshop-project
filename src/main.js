d12fd37554c0e965baf6cbfea34b22f74fc7c8f6
Thially Moura <thiallymoura@hotmail.com>
2025-04-11 14:44:10 -0300

    feat: responsive design

--- src/main.js
+++ src/main.js
@@ -5,9 +5,11 @@ import "./libs/dayjs.js";
 
 //css 
 import './styles/global.css';
+import './styles/media.css';
 import './styles/main.css';
 import './styles/modal.css';
 
 // js
 import "./modules/schedule.js";
-
+import "./modules/schedules/cancel.js";
+import "./modules/page-load.js";
